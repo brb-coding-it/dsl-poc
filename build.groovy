ecc2fd0c5756a770177646b7992770e318c7c2be
Holly Murphy <holly_murphy@gap.com>
2022-04-19 10:42:13 -0500

    initial commit

---
+++ build.groovy
@@ -0,0 +1,23 @@
+def gitUrl = "https://github.com/brb-coding-it/dsl-poc.git"
+
+job("dsl-build") {
+    description "Builds jobs from master branch."
+    parameters {
+        stringParam('COMMIT', 'HEAD', 'Commit to build')
+    }
+    scm {
+        git {
+            remote {
+                url gitUrl.
+                branch "origin/main"
+            }
+            extensions {
+                wipeOutWorkspace()
+                localBranch main
+            }
+        }
+    }
+    steps {
+        shell "Look: I'm building main!"
+    }
+}
